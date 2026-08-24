9d11cc81988bd46efbad955498f11be204a290e7
Hasika <hasikaoreo@gmail.com>
2026-08-24 13:47:21 +0530

    Implement 0/1 Knapsack problem in C++

---
+++ Week-4/Q4_01_Knapsack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int knapsack(int capacity, vector<int>& weights, vector<int>& values, int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 1; w <= capacity; w++) {
+
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                    dp[i - 1][w]
+                );
+            }
+            else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+int main() {
+    vector<int> weights = {2, 3, 4, 5};
+    vector<int> values = {3, 4, 5, 6};
+
+    int capacity = 5;
+
+    int n = weights.size();
+
+    int maximumValue = knapsack(capacity, weights, values, n);
+
+    cout << "Maximum value: " << maximumValue << endl;
+
+    return 0;
+}
